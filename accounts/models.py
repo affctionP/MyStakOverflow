53a631fd5ea38745077fe45bd09dec74c0a4a971
affctionP <affctionhossiny@yahoo.com>
2021-12-07 18:53:54 +0330

    add accounts app and custome user model

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import CustomUserManager
+# Create your models here.
+
+class USER (AbstractUser):
+    name=models.CharField(max_length=30,default="کاربر")
+    
+    username=None
+    email =models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS=[]
+    objects=CustomUserManager()
+    
+    def __str__(self):
+        return self.email
